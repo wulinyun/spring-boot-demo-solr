8a743212dc6d19bb2eb3963dcb48eed84d8abe39
wulinyun <lin.wu@landasoft.com>
2019-02-28 09:49:04 +0800

    项目初始化代码

---
+++ src/main/java/com/landasoft/mas/demo/solr/SpringBootDemoSolrApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.mas.demo.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoSolrApplication.class, args);
+	}
+
+}
